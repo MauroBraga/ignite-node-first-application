1765b0ee9eb27e4b8f45ccdf2e01dad21eb0aaa5
Mauro <mrb030584@yahoo.com>
2021-03-18 10:02:02 -0300

    cadastro de conta

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const { v4 : uuidv4} = require('uuid');
 
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.post('/account', (request, response) => {
+    const {cpf, name} = request.body;
+    const id = uuidv4();
+
+    customer = {id, cpf, name, statement: []} ;
+    customers.push(customer);
+
+    return response.status(201).send();
+});
 
 app.listen(3333);
