1292b00e812a276f5c2a33e8c31f4a95246d340e
Mauro <mrb030584@yahoo.com>
2021-03-18 15:55:31 -0300

    Criando deposito na conta

--- src/index.js
+++ src/index.js
@@ -42,4 +42,18 @@ app.get("/statement/", verifyIfExistsAccountCPF,(request, response) => {
     response.json(customer.statement);
 });
 
+app.post("/deposit/", verifyIfExistsAccountCPF,(request, response) => {
+    const {description, amount} = request.body;
+    const  {customer } = request;
+    
+    const statementOperation = {
+        description, 
+        amount,
+        created_at: new Date(),
+        type:'credit'
+    };
+    customer.statement.push(statementOperation);
+    response.status(201).json();
+ });
+
 app.listen(3333);
